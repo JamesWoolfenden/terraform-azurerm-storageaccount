c2d638b1fcbcd3a1cd4145aa57a3ae1ccaf065ef
James Woolfenden <james.woolfenden@gmail.com>
2020-10-20 16:54:30 +0100

    Update azurerm_stoage_account.store.tf

--- azurerm_stoage_account.store.tf
+++ azurerm_stoage_account.store.tf
@@ -7,6 +7,7 @@ resource "azurerm_storage_account" "store" {
   account_kind              = var.account_kind
   account_tier              = var.account_tier
   account_replication_type  = var.account_replication_type
+  min_tls_version           = "TLS1_2"
   enable_https_traffic_only = true
   is_hns_enabled            = var.is_hns_enabled
   tags                      = var.common_tags
